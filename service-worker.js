96e48a204c1d615156f0e402e5c91a3b8623c84c
foundlev <lev.lsb56@yandex.ru>
2024-07-18 11:11:50 +0300

    Fix PWA

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('pwa-cache').then((cache) => {
       return cache.addAll([
-        '/assets/',
+        '/icons/',
         '/manifest.json'
       ]);
     })
